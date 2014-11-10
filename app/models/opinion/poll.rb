059a27138ba1ee3fa328db92a4db7ea29d6b23a6
haiko <haiko@hotmail.com>
2014-11-10 23:18:09 +0100

    Added validation making sure that at least two options are defined.

--- app/models/opinion/poll.rb
+++ app/models/opinion/poll.rb
@@ -5,14 +5,22 @@ module Opinion
 		validates :state, :presence => true
 		validates :state, :inclusion => %w(active pending ended)
 		validates :question, :presence => true
-		# TODO at least two options need to be defined.
+
 		validates :options, :presence => true
+		validate :at_least_two_options
 
 		accepts_nested_attributes_for :options
 
 		scope :active, -> { where state: 'active' }
 		scope :pending, -> { where state: 'pending' }
 		scope :ended,  -> { where state: 'ended' }
+
+		# TODO Comment me.
+		def at_least_two_options
+			if options.size < 2
+				self.errors.add(:options, 'at least two must be defined')
+			end
+		end
 
 		# TODO Comment me.
 		def voted_by?(user)
